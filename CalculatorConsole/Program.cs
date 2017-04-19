ffa9f5d5340ebe742561ecf6f64f4842d3fde8e7
James Tasker <james.tasker@interact-intranet.com>
2017-04-19 18:54:03 +0100

    Add console application

---
+++ CalculatorConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MathsParser;
+
+namespace CalculatorConsole
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var parser = new Parser();
+
+			while (true)
+			{
+				Console.WriteLine("Please enter an expression to calculate:");
+				var input = Console.ReadLine();
+
+				if (string.IsNullOrEmpty(input))
+				{
+					break;
+				}
+
+				Console.WriteLine($"{input} = {parser.Parse(input)}");
+				Console.WriteLine();
+			}
+		}
+	}
+}
