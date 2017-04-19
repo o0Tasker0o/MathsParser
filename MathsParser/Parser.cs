251e42c48ba47e7a73d99fc74225bfa072850a9e
James Tasker <james.tasker@interact-intranet.com>
2017-04-19 13:07:33 +0100

    Parse basic Abs input

--- MathsParser/Parser.cs
+++ MathsParser/Parser.cs
@@ -1,6 +1,24 @@
-﻿namespace MathsParser
+﻿using System;
+
+namespace MathsParser
 {
 	public class Parser
 	{
+		public float Parse(string expression)
+		{
+			var openingBracketPosition = expression.IndexOf("(", StringComparison.InvariantCulture) + 1;
+			var closingBracketPosition = expression.IndexOf(")", openingBracketPosition, StringComparison.InvariantCulture);
+
+			var functionInput = expression.Substring(openingBracketPosition, closingBracketPosition - openingBracketPosition);
+
+			float inputValue;
+
+			if (!float.TryParse(functionInput, out inputValue))
+			{
+				throw new ArgumentException($"{nameof(expression)} must be a valid number");
+			}
+
+			return Math.Abs(float.Parse(functionInput));
+		}
 	}
 }
--- MathsParserTests/ParserTests.cs
+++ MathsParserTests/ParserTests.cs
@@ -1,9 +1,33 @@
-﻿using NUnit.Framework;
+﻿using System;
+using MathsParser;
+using NUnit.Framework;
 
 namespace MathsParserTests
 {
 	[TestFixture]
 	public class ParserTests
 	{
+		private Parser _parser;
+
+		[SetUp]
+		public void Setup()
+		{
+			_parser = new Parser();
+		}
+
+		[TestCase("1")]
+		[TestCase("1.0")]
+		[TestCase("-1")]
+		[TestCase("-1.0")]
+		public void ParseReturnsAbsoluteValueOfInput(string input)
+		{
+			Assert.AreEqual(1.0f, _parser.Parse($"Abs({input})"));
+		}
+
+		[Test]
+		public void ParseThrowsExceptionForNonNumericInputs()
+		{
+			Assert.Throws<ArgumentException>(() => _parser.Parse("Abs(THIS IS NOT A NUMBER)"));
+		}
 	}
 }
